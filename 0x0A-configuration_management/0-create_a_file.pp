523ed728940997ee202f7927e3d39e3736e2ccbe
magnifiscence <www.divinedivinepraise@gmail.com>
2023-11-24 07:13:25 -0800

    task done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-#Creates a file with the content "I love puppet" in /tmp/school
+# Creates a file with the content "I love Puppet" in /tmp/school
 file { '/tmp/school':
   ensure  => 'file',
-  content => 'file',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
